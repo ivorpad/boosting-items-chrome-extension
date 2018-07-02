95c4d25b1cefcac0895047c66ccb2ce31459f431
ivorpad <ivorjpc@gmail.com>
2018-07-02 17:13:07 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const proofingActionForms = document.querySelector(".proofing-action-forms");
+
+const newDiv = document.createElement("div");
+
+newDiv.id = "root";
+
+proofingActionForms.prepend(newDiv);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
